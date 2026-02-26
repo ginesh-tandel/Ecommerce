5649fd515a2c0c4b4d292332acb9a4159582b2fb
gines <gines@GINESH>
2026-02-26 19:00:47 +0530

    Add project files.

---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Specifications;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Pagination<Product>> GetProducts(CatalogSpecParams specParams);
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductsByBrandAsync(string name);
+        Task<Product> GetProductByIdAsync(string id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> UpdateProductAsync(Product product);
+        Task<Product> DeleteProductAsync(string productId);
+        Task<ProductBrand> GetBrandByIdAsync(string brandId);
+        Task<ProductType> GetTypeByIdAsync(string typeId);
+    }
+}
